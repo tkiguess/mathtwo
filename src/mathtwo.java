069bf35cb25daaca2523885b925327b111dffaf9
Tafkir Ahmed <ahmedtafkir@gmail.com>
2018-07-12 10:20:59 -0400

    uploading

---
+++ src/mathtwo.java
@@ -0,0 +1,12 @@
+public class mathtwo {
+
+        public static void main( String[] args )
+        {
+           int mynumber = 2;
+           double myothernumber = 1.7938;
+
+
+           System.out.println (mynumber + myothernumber);
+        }
+
+}
